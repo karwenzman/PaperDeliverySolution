e3388e878241b900e036c8690961686a52ec2e06
karwenzman <jenning.thorsten@gmx.net>
2023-12-05 11:28:43 +0100

    Initial Setup
    Basic projects and references.

---
+++ PaperDeliveryLibraryTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PaperDeliveryLibraryTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
