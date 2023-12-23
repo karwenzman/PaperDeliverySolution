e3b30650676f429cf9fcd670a12c27a172264e1a
karwenzman <jenning.thorsten@gmx.net>
2023-12-23 15:07:45 +0100

    #5 Refactoring
    Adds minor changes. Addes login icons.

--- PaperDeliveryLibrary/Repositories/IUserRepository.cs
+++ PaperDeliveryLibrary/Repositories/IUserRepository.cs
@@ -10,7 +10,6 @@ namespace PaperDeliveryWpf.Repositories
         /// </summary>
         /// <param name="login">The user's unique login expression.</param>
         /// <param name="password">The user's password.</param>
-        /// <param name="applicationOptions">The application options stored in configuration file.</param>
         /// <param name="databaseOptions">The database options stored in configuration file.</param>
         /// <returns>Null - if no valid user is found.</returns>
         UserModel? Login(string login, string password, IDatabaseOptions? databaseOptions = null);
