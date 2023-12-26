7d90eb329a598d83a77c148d72c991ca34036108
karwenzman <jenning.thorsten@gmx.net>
2023-12-26 10:16:30 +0100

    #5 LoginView PasswordBox
    Adds the funcionallity of PasswordBox.

---
+++ PaperDeliveryWpf/UserControls/Components/BindablePasswordBox.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PaperDeliveryWpf.UserControls.Components;
+
+public partial class BindablePasswordBox : UserControl
+{
+    private bool _isPasswordChanging;
+
+    public string Password
+    {
+        get { return (string)GetValue(PasswordProperty); }
+        set { SetValue(PasswordProperty, value); }
+    }
+
+    public static readonly DependencyProperty PasswordProperty =
+        DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox), new PropertyMetadata(string.Empty, PasswordPropertyChanged));
+
+
+    public BindablePasswordBox()
+    {
+        InitializeComponent();
+    }
+
+    private void BindablePasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        _isPasswordChanging = true;
+        Password = passwordBox.Password;
+        _isPasswordChanging = false;
+    }
+
+    private static void PasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is BindablePasswordBox passwordBox)
+        {
+            passwordBox.UpdatePassword();
+        }
+    }
+
+    private void UpdatePassword()
+    {
+        if (!_isPasswordChanging)
+        {
+            passwordBox.Password = Password;
+        }
+    }
+}
