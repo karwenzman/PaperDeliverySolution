159fe9a8e4daadbf0917cf34d4413dbd6be1f961
karwenzman <jenning.thorsten@gmx.net>
2023-12-29 11:45:04 +0100

    #5 Refactoring AccountViewModel
    Prepares the viewmodel to move methods to viewmodelbase.

--- PaperDeliveryWpf/ViewModels/AccountViewModel.cs
+++ PaperDeliveryWpf/ViewModels/AccountViewModel.cs
@@ -14,13 +14,10 @@ namespace PaperDeliveryWpf.ViewModels;
 
 public partial class AccountViewModel : ViewModelBase, IAccountViewModel
 {
+    private UserModel? _currentUser;
+
     private string? _displayName;
     private string? _email;
-
-    [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(SaveChangesButtonCommand))]
-    [NotifyCanExecuteChangedFor(nameof(DiscardChangesButtonCommand))]
-    private bool _currentUserHasChanged;
 
     [Required(ErrorMessage = "Enter your display name!")]
     public string? DisplayName
@@ -69,27 +66,30 @@ public partial class AccountViewModel : ViewModelBase, IAccountViewModel
     }
 
     [ObservableProperty]
-    private string? _password; // can only be modified via a new view
+    [NotifyCanExecuteChangedFor(nameof(SaveChangesButtonCommand))]
+    [NotifyCanExecuteChangedFor(nameof(DiscardChangesButtonCommand))]
+    private bool _currentUserHasChanged;
 
     [ObservableProperty]
-    private string? _role; // can only be modified by admin in accounts view
+    private string? _password;
 
     [ObservableProperty]
-    private bool _isActive; // can only be modified by admin in accounts view
+    private string? _role;
 
     [ObservableProperty]
-    private string? _userName; // can not be modified by user
+    private bool _isActive;
 
     [ObservableProperty]
-    private string? _lastLogin; // can not be modified by user
+    private string? _userName;
 
     [ObservableProperty]
-    private string? _lastModified; // can not be modified by user
+    private string? _lastLogin;
 
     [ObservableProperty]
-    private int _id; // can not be modified by user
+    private string? _lastModified;
 
-    private UserModel? _currentUser = new();
+    [ObservableProperty]
+    private int _id;
 
     private readonly ILogger<AccountViewModel> _logger;
     private readonly IUserRepository _userRepository;
@@ -100,23 +100,12 @@ public partial class AccountViewModel : ViewModelBase, IAccountViewModel
         _userRepository = userRepository;
         _currentUser = _userRepository.GetByUserName(GetUserName());
 
-        if (!IsUserInRole("user"))
-        {
-            _logger.LogError("** Access denied on page {class} by {name}!", nameof(AccountViewModel), GetUserName());
-            throw new ArgumentException($"Access denied on page {nameof(AccountViewModel)} by {GetUserName()}!");
-        }
-
-        if (_currentUser == null)
-        {
-            _logger.LogError("** No valid user account is loaded while accessing {class} by {name}!", nameof(AccountViewModel), GetUserName());
-            throw new ArgumentException($"No valid user account is loaded while accessing class {nameof(AccountViewModel)} by {GetUserName()}!");
-        }
-
-        _logger.LogInformation("* Loading {class}", nameof(AccountViewModel));
-
-        AssignUserAccountToLocalProperties();
+        CheckUserRole("user");
+        CheckUserAccountAndAssignToLocalProperties();
 
         CurrentUserHasChanged = false;
+
+        _logger.LogInformation("* Loading {class}", nameof(AccountViewModel));
     }
 
     #region ***** RelayCommand *****
@@ -163,7 +152,7 @@ public partial class AccountViewModel : ViewModelBase, IAccountViewModel
             {
                 message = "Update successful.\nThe changes have been saved to your account.";
                 _currentUser = _userRepository.GetByUserName(GetUserName());
-                AssignUserAccountToLocalProperties();
+                CheckUserAccountAndAssignToLocalProperties();
             }
             else
             {
@@ -204,22 +193,36 @@ public partial class AccountViewModel : ViewModelBase, IAccountViewModel
     }
     #endregion ***** End OF RelayCommand *****
 
-    private void AssignUserAccountToLocalProperties()
+    private void CheckUserRole(string userRole)
+    {
+        if (!IsUserInRole(userRole))
+        {
+            _logger.LogError("** Access denied on page {class} by {name}!", nameof(AccountViewModel), GetUserName());
+            throw new ArgumentException($"Access denied on page {nameof(AccountViewModel)} by {GetUserName()}!");
+        }
+    }
+
+    private void CheckUserAccount()
     {
         if (_currentUser == null)
         {
             _logger.LogError("** No valid user account is loaded while accessing {class} by {name}!", nameof(AccountViewModel), GetUserName());
             throw new ArgumentException($"No valid user account is loaded while accessing class {nameof(AccountViewModel)} by {GetUserName()}!");
         }
+    }
 
-        Id = _currentUser.Id;
-        IsActive = _currentUser.IsActive;
-        UserName = _currentUser.UserName;
-        DisplayName = _currentUser.DisplayName;
-        Role = _currentUser.Role;
-        Password = _currentUser.Password;
-        Email = _currentUser.Email;
-        LastLogin = _currentUser.LastLogin;
-        LastModified = _currentUser.LastModified;
+    private void CheckUserAccountAndAssignToLocalProperties()
+    {
+        CheckUserAccount();
+
+        Id = _currentUser!.Id;
+        IsActive = _currentUser!.IsActive;
+        UserName = _currentUser!.UserName;
+        DisplayName = _currentUser!.DisplayName;
+        Role = _currentUser!.Role;
+        Password = _currentUser!.Password;
+        Email = _currentUser!.Email;
+        LastLogin = _currentUser!.LastLogin;
+        LastModified = _currentUser!.LastModified;
     }
 }
